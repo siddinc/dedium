b389cd38ae7cd678ef5b341f62adaaf6a52df99a
Siddharth Sanghavi <sanghavisidd@gmail.com>
2018-07-24 01:57:09 +0530

    First Commit

---
+++ mysite/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.conf.urls import url, include
+from blog import views, forms
+from django.contrib.auth import views
+
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'', include('blog.urls')),
+    url(r'accounts/login/$', views.login, name = 'login'),
+    url(r'accounts/logout/$', views.logout, name = 'logout', kwargs = {'next_page': '/'}),
+]
